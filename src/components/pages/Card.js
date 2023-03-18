0704f3c8b7b8f79ea890cbe3c59a079b7bbbaf1c
Julien Claivaz <julienclaivaz@192.168.1.101>
2023-03-18 11:39:59 +0000

    layout portfolio pages

--- src/components/pages/Card.js
+++ src/components/pages/Card.js
@@ -3,12 +3,12 @@ import React from 'react';
 function Card(props) {
 
     return(
-        <div className="card" style={{width: "18rem"}}>
+        <div className="card mb-5 mx-auto" style={{width: "25rem"}}>
 <img className="card-img-top" src={props.img} alt="Card image cap"/>
 <div className="card-body">
   <h5 className="card-title">{props.name}</h5>
-  <p className="card-text">{props.descri}</p>
-  <a href={props.linkDeploy} className="card-link">The Project</a>
+  <p className="card-text">{props.description}</p>
+  <a href={props.linkLive} className="card-link float-left">The Project</a>
     <a href={props.linkRepo} className="card-link">The code</a>
 </div>
 </div>
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,12 +1,13 @@
 import React from "react";
+import Card from "./Card"
 import project from "../../project.json"
 
 function Portfolio() {
   console.log(project)
   return (
     <div>
-      <h1>Portfolio Page</h1>
-      <Card/>
+      <h1 class="mt-3 mb-4 text-center">My projects</h1>
+     {project.map(card => <Card {...card} key={card.id}/>)}
 </div>
   );
 }
